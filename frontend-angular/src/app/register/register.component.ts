431225a5576ed303b6e167fac1e60940a270264a
Zeitrom <hugo.verley@gmail.com>
2023-12-31 18:32:25 +0100

    modif register

--- frontend-angular/src/app/register/register.component.ts
+++ frontend-angular/src/app/register/register.component.ts
@@ -1,4 +1,33 @@
+// register.component.ts
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent {
+  user = {
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: '' // Assurez-vous que cette ligne est présente dans votre code
+  };
+
+  constructor(private router: Router) {}
+
+  onSubmit(): void {
+    // Logique de l'inscription
+
+    // Redirection vers la page d'accueil
+    this.router.navigate(['/']);
+
+    // Affichage de la notification de succès
+    alert('Inscription réussie ! Vous êtes bien inscrit.');
+  }
+}
+/*import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from '../user.service'; // Assurez-vous que le chemin est correct
 
@@ -37,4 +66,4 @@ export class RegisterComponent {
       }
     });
   }
-}
+}*/
